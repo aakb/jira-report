52b7a01acd8e96a1826a34c7d7a8de1abe5c8226
Troels Ugilt Jensen <tujensen@gmail.com>
2018-06-07 14:05:47 +0200

    TEAMMANGE-665: Started on sprint report

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,10 +7,141 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/sprint_report")
+     * @Method("GET")
+     */
+    public function sprintReportListAction(JiraService $jira) {
+        try {
+            $projects = $jira->get('/rest/api/2/project');
+            return $this->render('jira/sprint_report_list.html.twig', array(
+                    'projects' => $projects,
+                )
+            );
+        }
+        catch (HttpException $e) {
+            return new RedirectResponse('/login');
+        }
+    }
+
+    /**
+     * @Route("/sprint_report/project/{pid}")
+     * @Method("GET")
+     */
+    public function sprintReportAction(JiraService $jira, $pid) {
+        try {
+            $project = $jira->get('/rest/api/2/project/' . $pid);
+            return $this->render('jira/sprint_report.html.twig', array(
+                    'project' => $project,
+                )
+            );
+        }
+        catch (HttpException $e) {
+            return new RedirectResponse('/login');
+        }
+    }
+
+    /**
+     * @Route("/sprint_report/version/{vid}")
+     * @Method("GET")
+     */
+    public function sprintReportVersionAction(JiraService $jira, $vid) {
+        try {
+            $version = $jira->get('/rest/api/2/version/' . $vid);
+            $project = $jira->get('/rest/api/2/project/' . $version->projectId);
+
+            // Get all issues for version.
+            $issues = [];
+            $startAt = 0;
+            while (true) {
+                $results = $jira->get(
+                    '/rest/api/2/search' .
+                    '?jql=fixVersion=' . $vid .
+                    '&project=' . $version->projectId .
+                    '&maxResults=50' .
+                    '&fields=timetracking,worklog,timespent,timeoriginalestimate,summary,assignee,status,resolutionDate,customfield_10006' .
+                    '&startAt=' . $startAt);
+                $issues = array_merge($issues, $results->issues);
+
+                $startAt = $startAt + 50;
+
+                if ($results->total < $startAt) {
+                    break;
+                }
+            }
+
+            foreach ($issues as $issue) {
+                $issue->sprints = [];
+
+                foreach($issue->fields->customfield_10006 as $sprintString) {
+                    $replace = preg_replace(['/.*\[/', '/].*/'], '', $sprintString);
+                    $fields = explode(',', $replace);
+
+                    $sprint = [];
+                    foreach ($fields as $field) {
+                        $split = explode('=', $field);
+
+                        $sprint[$split[0]] = $split[1];
+                    }
+
+                    $issue->sprints[] = (object)$sprint;
+                }
+            }
+
+            // Get active sprint.
+            $boardId = 65;
+            $activeSprint = $jira->get('/rest/agile/1.0/board/' . $boardId . '/sprint?state=active');
+            $activeSprint = count($activeSprint->values) > 0 ? $activeSprint->values[0] : null;
+
+            // Get all sprints.
+            $allSprints = [];
+            $startAt = 0;
+            while (true) {
+                $results = $jira->get('/rest/agile/1.0/board/' . $boardId . '/sprint?startAt='.$startAt);
+                $allSprints = array_merge($allSprints, $results->values);
+
+                $startAt = $startAt + 50;
+
+                if ($results->isLast) {
+                    break;
+                }
+            }
+
+            $spentSum = 0;
+            $remainingSum = 0;
+            foreach ($issues as $issue) {
+                $spentSum = $spentSum + $issue->fields->timespent;
+
+                if (!$issue->fields->status->name != 'Done' && isset($issue->fields->timetracking->remainingEstimateSeconds)) {
+                    $remainingSum = $remainingSum + $issue->fields->timetracking->remainingEstimateSeconds;
+                }
+            }
+            $spentHours = $spentSum / 60 / 60;
+            $remainingHours = $remainingSum / 60 / 60;
+
+            return $this->render('jira/sprint_report_version.html.twig', array(
+                    'version' => $version,
+                    'project' => $project,
+                    'issues' => $issues,
+                    'activeSprint' => $activeSprint,
+                    'allSprints' => $allSprints,
+                    'spentSum' => $spentSum,
+                    'spentHours' => $spentHours,
+                    'remainingHours' => $remainingHours,
+                )
+            );
+        }
+        catch (HttpException $e) {
+            return new RedirectResponse('/login');
+        }
+    }
+
   /**
    * @Route("/")
    * @Method("GET")
--- src/Service/JiraService.php
+++ src/Service/JiraService.php
@@ -8,58 +8,85 @@ use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Subscriber\Oauth\Oauth1;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
-class JiraService {
-  protected $token_storage;
-  protected $customer_key;
-  protected $pem_path;
-  protected $jira_url;
+class JiraService
+{
+    protected $token_storage;
+    protected $customer_key;
+    protected $pem_path;
+    protected $jira_url;
 
-  /**
-   * Constructor.
-   */
-  public function __construct($token_storage, $customer_key, $pem_path, $jira_url) {
-    $this->token_storage = $token_storage;
-    $this->customer_key = $customer_key;
-    $this->pem_path = $pem_path;
-    $this->jira_url = $jira_url;
-  }
-
-  public function get($path) {
-    $stack = HandlerStack::create();
-    $token = $this->token_storage->getToken();
-    $middleware = $this->setOauth($token);
-
-    $stack->push($middleware);
-
-    $client = new Client([
-      'base_uri' => $this->jira_url,
-      'handler' => $stack
-    ]);
-
-    // Set the "auth" request option to "oauth" to sign using oauth
-    try {
-      $response = $client->get($path, ['auth' => 'oauth']);
-
-      if ($body = $response->getBody()) {
-
-        return json_decode($body);
-      }
-    } catch (RequestException $e) {
-        throw $e;
+    /**
+     * Constructor.
+     */
+    public function __construct(
+        $token_storage,
+        $customer_key,
+        $pem_path,
+        $jira_url
+    ) {
+        $this->token_storage = $token_storage;
+        $this->customer_key = $customer_key;
+        $this->pem_path = $pem_path;
+        $this->jira_url = $jira_url;
     }
-  }
 
-  public function setOauth($token) {
-    $middleware = new Oauth1([
-      'consumer_key'    => $this->customer_key,
-      'private_key_file' => $this->pem_path,
-      'private_key_passphrase' => '',
-      'signature_method' => Oauth1::SIGNATURE_METHOD_RSA,
-      'token'           => $token->getAccessToken(),
-      'token_secret'    => $token->getTokenSecret(),
-    ]);
+    public function get($path)
+    {
+        $stack = HandlerStack::create();
+        $token = $this->token_storage->getToken();
 
-    return $middleware;
-  }
+        if ($token instanceof AnonymousToken) {
+            throw new HttpException(401, 'unauthorized');
+        }
+
+        $middleware = $this->setOauth($token);
+
+        $stack->push($middleware);
+
+        $client = new Client(
+            [
+                'base_uri' => $this->jira_url,
+                'handler' => $stack,
+            ]
+        );
+
+        // Set the "auth" request option to "oauth" to sign using oauth
+        try {
+            $response = $client->get($path, ['auth' => 'oauth']);
+
+            if ($body = $response->getBody()) {
+                return json_decode($body);
+            }
+        } catch (RequestException $e) {
+            throw $e;
+        }
+    }
+
+    public function setOauth($token)
+    {
+        $accessToken = null;
+        $accessTokenSecret = null;
+
+        if (!$token instanceof AnonymousToken) {
+            $accessToken = $token->getAccessToken();
+            $accessTokenSecret = $token->getTokenSecret();
+        }
+
+        $middleware = new Oauth1(
+            [
+                'consumer_key' => $this->customer_key,
+                'private_key_file' => $this->pem_path,
+                'private_key_passphrase' => '',
+                'signature_method' => Oauth1::SIGNATURE_METHOD_RSA,
+                'token' => $accessToken,
+                'token_secret' => $accessTokenSecret,
+            ]
+        );
+
+        return $middleware;
+    }
 }
